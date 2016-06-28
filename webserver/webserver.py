25b957c52f4b4288adf429266d00a30c5badf480
RaphaelLu <ronilu66@hotmail.com>
2016-06-29 00:07:20 +0900

    added webserver

---
+++ webserver/webserver.py
@@ -0,0 +1,41 @@
+from BaseHTTPServer import *
+
+class webserverHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			if self.path.endswith("/hello"):
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+
+				output = ""
+				output += "<html><body>Hello!</body></html>"
+				self.wfile.write(output)
+				print output
+				return
+			if self.path.endswith("/hola"):
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+
+				output = ""
+				output += "<html><body>&#161Hola <a href='/hello'>Back to Hello</a></body></html>"
+				self.wfile.write(output)
+				print output
+				return
+		except IOError:
+			self.send_error(404, "File not found %s" % self.path)
+
+
+def main():
+	try:
+		port = 8080
+		server = HTTPServer(('', port), webserverHandler)
+		print "web server running on port %s" % port
+		server.serve_forever()
+
+	except KeyboardInterrupt:
+		print "^C entered, stopping web server"
+		server.socket.close()
+ 
+main()
